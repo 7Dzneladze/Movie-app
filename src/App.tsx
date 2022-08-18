5b7bfa25873f241a7d439e4e52e55ac0647be5eb
7Dzneladze <7dzneladze@gmail.com>
2022-08-18 13:29:04 +0400

    dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useRoutes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import MovieListing from "./components/MovieListing";
 import MovieDetail from "./components/MovieDetail";
@@ -8,8 +8,8 @@ import Team from "./components/Team";
 function App() {
   return (
     <div>
+      <Header />
       <Container>
-        <Header />
         <Routes>
           <Route path="*" element={<MovieListing />} />
           <Route path="/details/:imdbID" element={<MovieDetail />} />
